5f4cf11652bf3a20c9b68601d0cee84f82add637
Mjolksaft <Kalla01@hotmail.se>
2023-03-26 22:15:49 +0200

    fixed screening time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { useStates } from './utilities/states';
 import MovieList from './MovieList';
 import MovieDetail from './MovieDetail';
 import { Routes, Route } from 'react-router-dom';
@@ -6,47 +7,46 @@ import { kebabify } from './utilities/kebabify';
 
 export default function App() {
 
-  const [screenings, setScreenings] = useState([])
-  const [movies, setMovies] = useState([])
-  const [filter, setFilter] = useState([])
+  const s = useStates('main', {
+    movies: [],
+    filter: [],
+    screenings: [],
+  });
 
   const filterGenre = useRef()
 
   useEffect(() => {
     (async () => {
-      // fetch alll movies from the REST api
       let screenings = await(await fetch('/api/screenings')).json()
       let movies = await (await fetch('/api/movies')).json();
-      // add a slug to be used in url routes to each movie
+
       for (let movie of movies) {
         movie.slug = kebabify(movie.title);
       }
-      // store the movies in our state variable
-      setMovies(movies)
-      setFilter(movies)
-      setScreenings(screenings)
+      s.movies = movies;
+      s.filter = movies;
+      s.screenings = screenings
     })();
   }, []);
 
-  const filterMovie = () => {
+  const test = () => {
     const genre = filterGenre.current.value
-    if(genre === '') {
-      setFilter(movies)
+    if (genre === '') {
+      s.filter = s.movies
     } else {
-      setFilter(movies.filter(movie => movie.description.categories.includes(genre)))
+      s.filter = s.movies.filter(movie => movie.description.categories.includes(genre))
     }
-    filterGenre.current.value = null
-    //else
+    // genre.filterGenre.current.value = ''
   }
 
-  return <> 
-  <Routes>
-    <Route path="/" element={<>
-    <input ref={filterGenre} type="text" />
-    <button onClick={filterMovie}> Filter Movie</button>
-      <MovieList movies={filter} screenings={screenings}/>
-    </>}></Route>
-    <Route path="/movie-detail/:slug" element={<MovieDetail movies={movies}/>} />
-  </Routes>
-</>;
+  return s.filter.length === 0 ? null : <>
+    <Routes>
+      <Route path="/" element={<>
+        <input ref={filterGenre} type="text" />
+        <button onClick={test}>Filter Genre</button>
+        <MovieList />
+      </>}></Route>
+      <Route path="/movie-detail/:slug" element={<MovieDetail />} />
+    </Routes>
+  </>;
 }
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,13 +1,18 @@
+import { useStates } from './utilities/states';
 import { Link } from 'react-router-dom';
 
-export default function MovieList({movies}, {screenings}) {
-  console.log(screenings);
-  return <>
-    {movies.map(({ slug, id, title, description: d }) => <Link
+export default function MovieList() {
+
+  const s = useStates('main');
+
+return <>
+    {s.filter.map(({ slug, id, title, description: d }) => <Link
       to={'/movie-detail/' + slug}>
       <div className="movie">
         <h3>{title}</h3>
         <img src={'https://cinema-rest.nodehill.se' + d.posterImage} />
+        <p>Screening Date {s.screenings[id].time.split('T')[0]}</p>
+        <p>Screening time {s.screenings[id].time.split('T')[1].replace('Z', '')}</p>
         <p>Length {d.length}</p>
         <hr />
       </div>
